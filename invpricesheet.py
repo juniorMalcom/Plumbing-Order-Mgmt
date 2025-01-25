6c26093684f2ae62c1f48e06c81ef7d86c0e6292
juniorMalcom <nicholasheruska@icloud.com>
2025-01-25 16:55:03 -0500

    Update invpricesheet.py

--- invpricesheet.py
+++ invpricesheet.py
@@ -1,6 +1,16 @@
+#PEX MATERIALS
 p_12pex = 2
 p_34pex = 5
 p_12pexcoupling = 0.1
 p_34pexcoupling = 0.13
 p_12pexelbow = 0.09
-print (p_12pex)
+
+#GENERAL MATERIALS (PRICE IS ROUGH ESTIMATE)
+P_toliet = 80
+P_waterheatershort = 400
+P_waterheaterlong 800
+p_bathtubsingle = 600
+p_bathtubmultiple = 800
+p_shower = 450
+p_handicapshower = 500
+p_showerhead = 15
