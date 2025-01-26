f87c6d9a1397fb4925819416796762aa5275cf01
juniorMalcom <nicholasheruska@icloud.com>
2025-01-26 08:14:37 -0500

    Create Jobcost.py

---
+++ Jobcost.py
@@ -0,0 +1,15 @@
+
+#Finishings $ per unit 
+shower_head_install = 5
+toliet_install = 15
+bathroom_sink_install = 15
+kitchen_sink_install = 15
+water_heater_install = 25
+dishwasher_install = 20
+oxbox_cover_install = 1
+#finishing_cost = (shower_head_install + toliet_install...)
+
+#LABOR HOURS BY TASK
+#foreman = variables (size of building, # of units, # of bathrooms)
+#slab_plumbing = variables((on bottom floor: bathrooms, units), area of building)
+
